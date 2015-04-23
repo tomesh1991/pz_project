d986bf63d3c268e0b22e353134743e541113fe46
gqc3k <rob.gaca@gmail.com>
2015-04-23 22:33:25 +0200

    Nowa wersja API

---
+++ Monitor/WebApiPZ/Controllers/hostsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebApiPZ.Models;
+
+namespace WebApiPZ.Controllers
+{
+    public class hostsController : ApiController
+    {
+        private pzProjDBEntities db = new pzProjDBEntities();
+
+        // GET api/hosts
+        public IQueryable<hosts> Gethosts()
+        {
+            return db.hosts;
+        }
+
+        // GET api/hosts/5
+        [ResponseType(typeof(hosts))]
+        public IHttpActionResult Gethosts(int id)
+        {
+            hosts hosts = db.hosts.Find(id);
+            if (hosts == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(hosts);
+        }
+
+        // PUT api/hosts/5
+        public IHttpActionResult Puthosts(int id, hosts hosts)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != hosts.id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(hosts).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!hostsExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST api/hosts
+        [ResponseType(typeof(hosts))]
+        public IHttpActionResult Posthosts(hosts hosts)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.hosts.Add(hosts);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (hostsExists(hosts.id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = hosts.id }, hosts);
+        }
+
+        // DELETE api/hosts/5
+        [ResponseType(typeof(hosts))]
+        public IHttpActionResult Deletehosts(int id)
+        {
+            hosts hosts = db.hosts.Find(id);
+            if (hosts == null)
+            {
+                return NotFound();
+            }
+
+            db.hosts.Remove(hosts);
+            db.SaveChanges();
+
+            return Ok(hosts);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool hostsExists(int id)
+        {
+            return db.hosts.Count(e => e.id == id) > 0;
+        }
+    }
+}
--- Monitor/readme.txt
+++
@@ -1,8 +0,0 @@
-Dla jasności : tak jest to cały projekt Visuala. Żeby ogarnąć jak działa to API i WEB service restowy musiałem takowy stworzyć i posiada on własną konfigurację dla bazy danych (niestety nie mam pliku naszej, więć musiałem lekko improwizować i stworzyłem póki co logikę (kontrollery, odpowiadające im obiekty itd...) dla użytkowników i hostów. 
-
-po odpaleniu ( ja mam VS 12  ale zalecana jest raczej 13+ - niektóre funkcje mi nie działają i straciłęm przez to sporo czasu bo da się trochę rzeczy robić mniej manualnie) 
-jeżeli pojawi się strona główna to można odpytywać serwer poprzez dopisanie 
-
-api/hosts api/hosts/1 itd
-api/users api/users/1 itd.
-
