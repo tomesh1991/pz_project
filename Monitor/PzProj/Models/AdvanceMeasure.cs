2b719b675ef81307b761a71fd0651ebc0b9cb504
mpabian <marcin.pabian@mp.pl>
2015-05-19 07:46:58 +0200

    adv measurement model

---
+++ Monitor/PzProj/Models/AdvanceMeasure.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PzProj.Models
+{
+    public class AdvanceMeasure
+    {
+        public User User { get; set; }
+        public SimpleMeasureType SimpleMeasureType { get; set; }
+        public TimeSpan MeasureLength { get; set; } //pomiar z np 5 minut
+        public TimeSpan MeasureFrequency {get; set; } //co minute
+    }
+}
