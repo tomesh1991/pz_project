f94653d040512ced27b754ce7d17246551fe035c
mpabian <marcin.pabian@mp.pl>
2015-05-22 06:57:28 +0200

    adv measure rest

--- Monitor/Controllers/AdvMeasureController.cs
+++ Monitor/Controllers/AdvMeasureController.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Http;
 using System.Web.Http.Description;
@@ -12,6 +15,108 @@ namespace PzProj.Controllers
     public class AdvMeasureController : ApiController
     {
         private PzProjContext db = new PzProjContext();
+
+
+        // GET api/AdvMeasurements
+        public IQueryable<AdvanceMeasure> GetAdvMeasurements()
+        {
+            return db.AdvanceMeasure;
+        }
+
+        // GET api/AdvMeasurements/5
+        [ResponseType(typeof(AdvanceMeasure))]
+        public IHttpActionResult GetAdvMeasurements(int id)
+        {
+            AdvanceMeasure AdvMeasurements = db.AdvanceMeasure.Find(id);
+            if (AdvMeasurements == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(AdvMeasurements);
+        }
+
+        // PUT api/AdvMeasurements/5
+        public IHttpActionResult PutAdvMeasurements(int id, AdvanceMeasure AdvMeasurements)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != AdvMeasurements.id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(AdvMeasurements).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AdvMeasurementsExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST api/AdvMeasurements
+        [ResponseType(typeof(AdvanceMeasure))]
+        public IHttpActionResult PostAdvMeasurements(AdvanceMeasure AdvMeasurements)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.AdvanceMeasure.Add(AdvMeasurements);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = AdvMeasurements.id }, AdvMeasurements);
+        }
+
+        // DELETE api/AdvMeasurements/5
+        [ResponseType(typeof(AdvanceMeasure))]
+        public IHttpActionResult DeleteAdvMeasurements(int id)
+        {
+            AdvanceMeasure AdvMeasurements = db.AdvanceMeasure.Find(id);
+            if (AdvMeasurements == null)
+            {
+                return NotFound();
+            }
+
+            db.AdvanceMeasure.Remove(AdvMeasurements);
+            db.SaveChanges();
+
+            return Ok(AdvMeasurements);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool AdvMeasurementsExists(int id)
+        {
+            return db.AdvanceMeasure.Count(e => e.id == id) > 0;
+        }
+
+
+
 
         // nie wiem czy działa, można uprościć
         /// <summary>
