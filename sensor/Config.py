ae8521493e1acc82a29f71f2d554ec1279639ac2
Satanik <krzysztof.stanik0@gmail.com>
2015-06-17 21:15:25 +0200

    Add install script for sensor;

--- sensor/Config.py
+++ sensor/Config.py
@@ -6,8 +6,7 @@ class Config:
         self.hostUniqueId = os.popen('wmic baseboard get serialnumber').read().split()[1]
         print(self.hostUniqueId)
     frequency = 1.0/60.0
-    monitorUrl = "http://localhost:52123/api/measurements"
-    name = "SUPAKOMPJUTA"
+    name = "PIRATO_NINJA"
     sensorId = randint(0,10000000)
-    # util/MEM - pamiec util/CPU - %CPU
-    sensorType = "util/MEM"
+    monitorUrl = "http://localhost:52123/api/measurements"
+    sensorType = "util/MEM" # util/MEM - pamiec util/CPU - %CPU
---
+++ sensor/install.bash
@@ -0,0 +1,25 @@
+declare -a arr=("CPU_sensor/" "MEM_sensor/")
+declare -a arr2=("util/CPU" "util/MEM")
+
+for ((a = 0 ; a < 2 ; a++ ));
+do
+   i=${arr[$a]}
+   echo "$i"
+   cp Config.py $i
+   cp sensor.py $i
+   cp SensorUtil.py $i
+   i=$i"Config.py"
+   printf "\n    monitorUrl = " >> $i 
+   printf '"' >> $i
+   printf $1 >> $i
+   printf '"' >> $i
+   printf "\n" >> $i
+   printf "    sensorType = " >> $i 
+   printf '"' >> $i
+   printf ${arr2[$a]}>> $i
+   printf '"' >> $i
+   printf "\n" >> $i
+   python.exe ${arr[$a]}"sensor.py"&
+done
+
+
